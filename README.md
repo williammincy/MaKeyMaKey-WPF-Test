b1bac4d61cc8204d8212141f4f184f070d8a3fab
William Mincy <will@wmdigital.com>
2015-02-21 17:25:54 -0500

    + Updated README file + Added simple WPF tester application

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MaKeyMaKey-WPF-Test
-Simple C# WPF Application to test MaKeyMakey
+A simple C# WPF Application to test MaKey Makey within WPF made in Visual Studio 2015 Preview. I have included a published installer along with the source code, just in case someone may want to run this on a Windows machine without having to compile.
+
+The application assumes that you have remapped the MaKey Makey with the following keys
+* Up
+* DOwn
+* Left 
+* Right
+* W
+* A 
+* S 
+* D 
+
+To remap the MaKey MaKey go to this URL: http://www.makeymakey.com/remap/
+
+When running the application with a MaKey MaKey connected the corresponding graphic will light up when its key is pressed. It will go back to a default state when the key is released.
---
+++ wmdigital.GTFO.Wpf/GTFO/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GTFO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush ActiveColor = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            GetOutlinePaths(e.Key).Fill = ActiveColor;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+           GetOutlinePaths(e.Key).Fill = null;
+        }
+
+        private Path GetOutlinePaths(Key key)
+        {
+            string outline = key.ToString() + "LetterOutline";
+            return (from r in ControlOutlines.Children.OfType<Path>()
+                                              where r.Name == key.ToString() + "Letter"
+                                              select r).FirstOrDefault<Path>();
+        }
+    }
+}
